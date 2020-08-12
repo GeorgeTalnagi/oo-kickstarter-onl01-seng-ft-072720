efd647471046074fbd753eae181245bd09440fa1
George Talnagi <gtalnagi@yahoo.com>
2020-08-12 17:28:43 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -11,5 +11,9 @@ attr_reader :title, :backers
   @backers << backer
   end 
 
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
 
 end 
